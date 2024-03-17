4a9a0bfd1f57585e88c1d4f22ce6e853fc3755e3
seung1116 <wjoo3@plitsoft.com>
2024-03-18 02:38:11 +0900

    feat : 상세 페이지 섹션 2

--- app/components/detail/detailSection2.tsx
+++ app/components/detail/detailSection2.tsx
@@ -1,9 +1,63 @@
-import React from "react";
+"use client";
 
-const DetailSection2 = () => {
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+interface DetailSection2Props {
+  imgSrc: string;
+  title: string[];
+  link?: string;
+}
+
+const DetailSection2 = ({ data }: { data: DetailSection2Props[] }) => {
+  const [isHover, setIsHover] = useState<number>(-1);
+
   return (
-    <section className="h-[67.5rem] flex flex-col justify-between items-center p-[4rem] bg-black text-white leading-normal mobile:h-[50.75rem] mobile:p-[2rem]">
-      DetailSection2
+    <section className="h-max flex justify-center items-center py-[7rem] bg-black text-white leading-normal mobile:h-[50.75rem] laptop:py-[5rem] mobile:py-[3rem]">
+      <div className="flex gap-2 flex-wrap w-[73rem] h-[29rem] overflow-y-auto mobile:w-[18.75rem] mobile:h-[38rem] laptop:w-[40.5rem] laptop:h-[31rem]">
+        {data.map((item, index) => (
+          <div
+            onMouseOver={() => setIsHover(index)}
+            onMouseLeave={() => setIsHover(-1)}
+            key={index}
+            className="relative w-[24rem] h-[14rem] bg-white mobile:w-[18.75rem] mobile:h-[9rem] laptop:w-[20rem] laptop:h-[10rem]"
+          >
+            {item.link ? (
+              <Link href={item.link} target="_blank">
+                <div className="absolute top-0 left-0 z-10 w-full h-full bg-gradient-to-b from-transparent to-black/[0.5]">
+                  <Image
+                    src="/svgs/whiteArrow.svg"
+                    alt=""
+                    width={20}
+                    height={20}
+                    className="absolute bottom-[1rem] right-[2rem] rotate-[45deg] mobile:w-[1rem] mobile:h-[1rem] mobile:right-4"
+                  />
+                  <div className="absolute bottom-4 left-8 flex flex-col font-pretendard text-[1rem] text-white mobile:text-[0.875rem] mobile:bottom-2 mobile:left-4">
+                    {item.title.map((t, index) => (
+                      <span key={index}>{t}</span>
+                    ))}
+                  </div>
+                </div>
+              </Link>
+            ) : (
+              <div className="absolute top-0 left-0 z-10 w-full h-full bg-gradient-to-b from-transparent to-black/[0.5]">
+                <div className="absolute bottom-4 left-8 flex flex-col font-pretendard text-[1rem] text-white mobile:text-[0.875rem] mobile:bottom-2 mobile:left-4">
+                  {item.title.map((t, index) => (
+                    <span key={index}>{t}</span>
+                  ))}
+                </div>
+              </div>
+            )}
+            <Image
+              src={`/imgs/${item.imgSrc}`}
+              alt=""
+              fill
+              className="object-contain"
+            />
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 import DetailSection1 from "@/app/components/detail/detailSection1";
 import DetailSection2 from "@/app/components/detail/detailSection2";
 import { detailSection1Data } from "@/app/data/detailSection1Data";
+import { detailSection2Data } from "@/app/data/detailSection2Data";
 
 const DetailPage = () => {
   const pathname = usePathname();
@@ -16,7 +17,7 @@ const DetailPage = () => {
         data={detailSection1Data[dataIndex - 1]}
         index={dataIndex}
       />
-      <DetailSection2 />
+      <DetailSection2 data={detailSection2Data[dataIndex - 1]} />
     </main>
   );
 };
