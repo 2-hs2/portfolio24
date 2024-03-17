109b379dfba7df10e396cd7cb648a8662b12b06c
seung1116 <wjoo3@plitsoft.com>
2024-03-17 23:07:01 +0900

    feat : 상세 페이지 파일 구성 및 목차 페이지 link 데이터 적용

--- app/data/indexListData.ts
+++ app/data/indexListData.ts
@@ -10,24 +10,24 @@ export const indexListData = [
     title: "파란학기제",
     titleSmall: ["Paran", " Project"],
     desc: "아주대학교의 학기성 프로젝트 ‘파란학기제'에서 ‘아주대학교 개발자들을 위한 웹 커뮤니티 제작’을 주제로 디자인 및 웹 퍼블리싱을 진행했습니다.",
-    link: "/",
+    link: "/detail/1",
   },
   {
     title: "래브래도랩스 인턴",
     titleSmall: ["LabradorLabs", "Intern"],
     desc: "4개월간의 래브라도랩스에서 프론트팀 인턴으로 실무를 경험했습니다. 소프트웨어 공급망 관리 플랫폼인 ‘래브라도'의 페이지 디자인 및 개발 등의 업무를 진행했습니다.",
-    link: "/",
+    link: "/detail/2",
   },
   {
     title: "미디어 프로젝트",
     titleSmall: ["Media", "Project"],
     desc: "아주대학교 미디어학과의 졸업 프로젝트인 미디어프로젝트에서 ‘디지털미디어 1인 예술가들을 위한 웹 전시회 개발’을 주제로 프로젝트의 기획, 디자인, 웹 퍼블리싱을 맡았습니다.",
-    link: "/",
+    link: "/detail/3",
   },
   {
     title: "플리트소프트 인턴",
     titleSmall: ["Plitsoft", "Intern"],
     desc: "6개월을 목표로 현재 플리트소프트의 프론트팀 인턴으로 활동 중에 있습니다. ‘운동센터 관리 플랫폼, SNPE’, ‘센터 내 결제 관리 플랫폼, SPAY’ 개발을 팀원들과 협업을 통해 진행 중에 있습니다.",
-    link: "/",
+    link: "/detail/4",
   },
 ];
