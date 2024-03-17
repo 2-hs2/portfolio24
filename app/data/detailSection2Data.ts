c90c91bc824f5276045ebc9b90177023af6a8f3a
seung1116 <wjoo3@plitsoft.com>
2024-03-18 01:43:15 +0900

    feat : 상세 페이지 섹션 2 데이터

---
+++ app/data/detailSection2Data.ts
@@ -0,0 +1,177 @@
+export const detailSection2Data = [
+  [
+    {
+      imgSrc: "paran1.png",
+      title: ["로그인/회원가입 페이지", "UI 디자인"],
+      link: "https://xd.adobe.com/view/1e444c23-ed9c-4272-8da1-f6441741554d-4b17/",
+    },
+    {
+      imgSrc: "paran2.png",
+      title: ["메인 페이지", "UI 디자인"],
+      link: "https://xd.adobe.com/view/1e444c23-ed9c-4272-8da1-f6441741554d-4b17/",
+    },
+    {
+      imgSrc: "paran3.png",
+      title: ["게시글 목록 페이지", "UI 디자인"],
+      link: "https://xd.adobe.com/view/1e444c23-ed9c-4272-8da1-f6441741554d-4b17/",
+    },
+    {
+      imgSrc: "paran4.png",
+      title: ["게시글 상세 페이지", "UI 디자인"],
+      link: "https://xd.adobe.com/view/1e444c23-ed9c-4272-8da1-f6441741554d-4b17/",
+    },
+    {
+      imgSrc: "paran5.png",
+      title: ["게시글 작성 페이지", "UI 디자인"],
+      link: "https://xd.adobe.com/view/1e444c23-ed9c-4272-8da1-f6441741554d-4b17/",
+    },
+    {
+      imgSrc: "paran6.png",
+      title: ["투두 리스트 페이지", "UI 디자인"],
+      link: "https://xd.adobe.com/view/1e444c23-ed9c-4272-8da1-f6441741554d-4b17/",
+    },
+    {
+      imgSrc: "paran7.png",
+      title: ["마이 페이지", "UI 디자인"],
+      link: "https://xd.adobe.com/view/1e444c23-ed9c-4272-8da1-f6441741554d-4b17/",
+    },
+    {
+      imgSrc: "paran7.png",
+      title: ["마이 페이지", "UI 디자인"],
+      link: "https://xd.adobe.com/view/1e444c23-ed9c-4272-8da1-f6441741554d-4b17/",
+    },
+    {
+      imgSrc: "paran8.png",
+      title: ["로고 디자인 후보"],
+      link: "https://assets.adobe.com/id/urn:aaid:sc:AP:97450c5d-747c-4020-ac0c-d3f703b80121?view=published",
+    },
+    {
+      imgSrc: "paran9.png",
+      title: ["사이트 색상 견본"],
+      link: "https://assets.adobe.com/id/urn:aaid:sc:AP:576805b7-d31a-484a-80e4-f9ff93101251?view=published",
+    },
+    {
+      imgSrc: "paran11.png",
+      title: ["중간 발표 자료"],
+      link: "https://drive.google.com/file/d/1St6e_hoHL8LMwJ4RhMiZ0ChGbvrxP-oj/view?usp=sharing",
+    },
+    {
+      imgSrc: "paran10.png",
+      title: ["최종 발표 자료"],
+      link: "https://drive.google.com/file/d/1ROT682ACHS08XL5N6hCrYxa7jzplxzyh/view?usp=sharing",
+    },
+    {
+      imgSrc: "paran12.png",
+      title: ["캐릭터 디자인"],
+      link: "https://assets.adobe.com/id/urn:aaid:sc:AP:ab32fe87-3cf2-488b-9f47-597b5542299d?view=published",
+    },
+  ],
+  [
+    {
+      imgSrc: "labrador5.png",
+      title: ["래브라도 사이트"],
+      link: "https://labrador.labradorlabs.ai/",
+    },
+    {
+      imgSrc: "labrador1.png",
+      title: ["분석 요약 페이지", "UI 디자인"],
+      link: "https://www.figma.com/file/wPyvTnfY3nNq3UDALngB8E/labrador-ui?type=design&node-id=14%3A79&mode=design&t=SMwZ4a6GgqLM6xmV-1",
+    },
+    {
+      imgSrc: "labrador2.png",
+      title: ["분석 비교 페이지", "UI 디자인"],
+      link: "https://www.figma.com/file/wPyvTnfY3nNq3UDALngB8E/labrador-ui?type=design&node-id=14%3A79&mode=design&t=SMwZ4a6GgqLM6xmV-1",
+    },
+    {
+      imgSrc: "labrador3.png",
+      title: ["래브라도 UI 분석 및 정리"],
+      link: "https://www.figma.com/file/wPyvTnfY3nNq3UDALngB8E/labrador-ui?type=design&node-id=14%3A79&mode=design&t=SMwZ4a6GgqLM6xmV-1",
+    },
+    {
+      imgSrc: "labrador4.png",
+      title: ["삼성화재 IVAS", "UI 리뉴얼"],
+      link: "https://www.figma.com/file/wPyvTnfY3nNq3UDALngB8E/labrador-ui?type=design&node-id=14%3A79&mode=design&t=SMwZ4a6GgqLM6xmV-1",
+    },
+    {
+      imgSrc: "labrador6.png",
+      title: ["인턴 마무리 발표", "Wrap Up"],
+      link: "https://drive.google.com/file/d/16Ezy5y1aLDvElMocrznNWKuIKzVwwigD/view?usp=sharing",
+    },
+  ],
+  [
+    {
+      imgSrc: "media1.png",
+      title: ["User Flow"],
+      link: "https://www.figma.com/file/OYiFcuTNL4U8S4L1T3bVMg/%EC%A0%84%EC%B2%B4-User-Flow?type=whiteboard&node-id=0%3A1&t=IMQbmJfa9mLWdD61-1",
+    },
+    {
+      imgSrc: "media5.png",
+      title: ["기능 명세서"],
+      link: "https://2hs2.notion.site/0f159428b146490d87306ec4bce358e9?v=9771aa2ba74c42dab5c46bcf9ff325a5&pvs=4",
+    },
+    {
+      imgSrc: "media9.png",
+      title: ["로고 디자인"],
+      link: "https://assets.adobe.com/id/urn:aaid:sc:AP:92c821ae-a140-4a73-ac32-ecf8bce97000?view=published",
+    },
+    {
+      imgSrc: "media2.png",
+      title: ["UI 와이어 프레임"],
+      link: "https://www.figma.com/file/HB4rHw5ClXm7xEhOYED75V/UI-WireFrame?type=design&node-id=0%3A1&mode=design&t=dzBuQXaqzaVjAAdh-1",
+    },
+    {
+      imgSrc: "media3.png",
+      title: ["UI 디자인"],
+      link: "https://www.figma.com/file/qcx68DBcvZgbvqGInxFHht/UI-Design?type=design&node-id=4%3A114&mode=design&t=dzBuQXaqzaVjAAdh-1",
+    },
+    {
+      imgSrc: "media4.png",
+      title: ["GitHub 링크"],
+      link: "https://github.com/23-2-Media-Project/front",
+    },
+    {
+      imgSrc: "media6.png",
+      title: ["1차 IDEA 발표"],
+      link: "https://2hs2.notion.site/95984a9e91234c2188032cf0f6c750a1?pvs=4",
+    },
+    {
+      imgSrc: "media7.png",
+      title: ["2차 Reference 분석 발표"],
+      link: "https://2hs2.notion.site/95984a9e91234c2188032cf0f6c750a1?pvs=4",
+    },
+    {
+      imgSrc: "media8.png",
+      title: ["3차 최종 발표"],
+      link: "https://2hs2.notion.site/95984a9e91234c2188032cf0f6c750a1?pvs=4",
+    },
+  ],
+  [
+    {
+      imgSrc: "plitsoft1.png",
+      title: ["플리트소프트 홈페이지", "리뉴얼 개발"],
+      link: "https://www.plitsoft.com/",
+    },
+    {
+      imgSrc: "plitsoft2.png",
+      title: ["SNPE 프로젝트", "프론트 기능 정의서"],
+      link: "https://2hs2.notion.site/f2808aba35dc408ab2d306cf1f8f6beb?pvs=4",
+    },
+    {
+      imgSrc: "plitsoft3.png",
+      title: ["SNPE 프로젝트", "데일리스크럼"],
+    },
+    {
+      imgSrc: "plitsoft5.png",
+      title: ["SNPE 프로젝트", "개발 화면 일부"],
+    },
+    {
+      imgSrc: "plitsoft4.png",
+      title: ["SNPE 프로젝트", "코드 개발 일부"],
+    },
+    {
+      imgSrc: "plitsoft6.png",
+      title: ["지인 어플리케이션", "소개 페이지 개발"],
+      link: "https://zinn.co.kr/",
+    },
+  ],
+];
